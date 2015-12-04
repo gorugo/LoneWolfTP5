e69c5995c028c540e21c84826ed86fb1e745aa1f
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-12-04 13:08:53 -0500

    Renamings

---
+++ public/javascript/controlleurs/controlleurStats.js
@@ -0,0 +1,20 @@
+var app = angular.module('stats', []);
+
+app.controller('controlleurStats', function($scope, $http) {
+
+  $scope.getJoueur = function () { 
+    $http.get('/api/joueurs/565c64518b7057980cb10be9').
+      success(function(data, status, headers, config) {
+
+      console.log("success!");
+      console.log(data);
+      $scope.joueur = data;
+    }).
+    error(function(data, status, headers, config) {
+      console.log("error");
+    });
+  }
+
+  $scope.getJoueur();
+});
+
