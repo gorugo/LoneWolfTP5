64fae42bc0bb9171036cf07599b1fe1fb57e9f9c
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-12-04 11:43:23 -0500

    Test angular webservice call

--- public/javascripts/controllers/test.js
+++ public/javascripts/controllers/test.js
@@ -2,9 +2,34 @@
 
 var app = angular.module('appTest', []);
 
-app.controller('controlleurTest', function($scope) {
-    $scope.leString = 'EZ';
-    $scope.salutation = { texte: 'Bonjour' };
+app.controller('controlleurTest', function($scope, $http) {
+
+  $scope.getTest = function () { 
+    $http.get('/api/combat/12/34/1/132').
+      success(function(data, status, headers, config) {
+
+        console.log("success!");
+        console.log(data);
+        $scope.getVar = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("error");
+      });
+
+    $http.get('/api/joueurs/565c64518b7057980cb10be9').
+      success(function(data, status, headers, config) {
+
+      console.log("success!");
+      console.log(data);
+      $scope.joueur = data;
+    }).
+    error(function(data, status, headers, config) {
+      console.log("error");
+    });
+  }
+
+  $scope.leString = 'EZ';
+  $scope.salutation = { texte: 'Bonjour' };
 });
 
 // app.controller('controlleurTest',['$scope','$http',function($scope,$http)
