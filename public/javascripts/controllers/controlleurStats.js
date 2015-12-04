9b08323bff199f7bffe33324fbf0571dbb3e772b
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-12-04 12:52:34 -0500

    Use angular to display Stats view

---
+++ public/javascripts/controllers/controlleurStats.js
@@ -0,0 +1,32 @@
+var app = angular.module('stats', []);
+
+app.controller('controlleurStats', function($scope, $http) {
+
+  $scope.getTest = function () { 
+    $http.get('/api/combat/12/34/1/132').
+      success(function(data, status, headers, config) {
+
+        console.log("success!");
+        console.log(data);
+        $scope.getVar = data;
+      }).
+      error(function(data, status, headers, config) {
+        console.log("error");
+      });
+
+    $http.get('/api/joueurs/565c64518b7057980cb10be9').
+      success(function(data, status, headers, config) {
+
+      console.log("success!");
+      console.log(data);
+      $scope.joueur = data;
+    }).
+    error(function(data, status, headers, config) {
+      console.log("error");
+    });
+  }
+  $scope.getTest();
+  $scope.leString = 'EZ';
+  $scope.salutation = { texte: 'Bonjour' };
+});
+
